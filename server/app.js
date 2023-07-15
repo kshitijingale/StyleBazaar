250bc7592aa9e3977246be3668cc187f5d38a51e
Kshitij Ingale <kshitijingale2@gmail.com>
2023-07-15 11:36:11 +0530

    Middlewares

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth')
+
 const app = express();
+
 // DB Connection
 require('./config/db').connect();
 
-app.use('/', (req, res) => {
-    res.send('T-shirt store')
-})
+// Middleware
+app.use(cors());
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/api', authRoutes);
 
 module.exports = app;
