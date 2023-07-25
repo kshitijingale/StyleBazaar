b6132bc2fa31cc4d0ad76fb3a5fb149d6a6b0c90
Kshitij Ingale <kshitijingale2@gmail.com>
2023-07-25 23:16:16 +0530

    API calls

--- client/src/admin/helper/adminapicall.js
+++ client/src/admin/helper/adminapicall.js
@@ -1,17 +1,99 @@
 import axios from "axios";
 import { API } from "../../backend";
 
-export const createCategory = (userId, token, category) => {
-    return axios.post(`${API}/category/${userId}`, category, {
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
+export const createCategory = async (userId, token, category) => {
+    try {
+        const response = await axios.post(`${API}/category/${userId}`, category, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
         }
+        );
+        return response;
+    } catch (err) {
+        return err.response;
     }
-    ).then(response => {
-        return response
-    }).catch(err => {
-        return err.response
-    })
+}
+
+export const getCategories = async () => {
+    try {
+        const response = await axios.get(`${API}/categories`);
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+}
+
+// Product calls
+
+// create a product
+
+export const createProduct = async (userId, token, product) => {
+    try {
+        const response = await axios.post(`${API}/product/create/${userId}`, product, {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+}
+
+// get products
+
+export const getProducts = async () => {
+    try {
+        const response = await axios.get(`${API}/products`);
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+}
+
+// get a product
+
+export const getProduct = async (productId) => {
+    try {
+        const response = await axios.get(`${API}/product/${productId}`);
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+}
+
+// delete a product
+
+export const deleteProduct = async (productId, userId, token) => {
+    try {
+        const response = await axios.delete(`${API}/product/${productId}/${userId}`, {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response;
+    } catch (err) {
+        return err.response;
+    }
+}
+
+// update a product
+
+export const updateProduct = async (productId, userId, token, product) => {
+    try {
+        const response = await axios.put(`${API}/product/${productId}/${userId}`, product, {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response;
+    } catch (err) {
+        return err.response;
+    }
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getUserById } = require('../controllers/user.controller');
 const { getCategoryById } = require('../controllers/category.controller');
-const { getProductById, createProduct, getProduct, photo, updateProduct, getProducts, getAllUniqueCategories } = require('../controllers/product.controller');
+const { getProductById, createProduct, getProduct, photo, updateProduct, getProducts, getAllUniqueCategories, removeProduct } = require('../controllers/product.controller');
 const { isSignedIn, isAuthenticated, isAdmin } = require('../middlewares/auth')
 
 router.param("userId", getUserById)
@@ -12,6 +12,7 @@ router.param("productId", getProductById)
 
 router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct)
 router.put('/product/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, updateProduct)
+router.delete('/product/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, removeProduct)
 router.get('/product/:productId', getProduct)
 router.get('/product/photo/:productId', photo)
 router.get('/products', getProducts)
